0400e2cb7e92c503e720f3d128604005a361e649
Jaco-Pool <jaco.pool@gmail.com>
2024-02-01 14:37:57 +0200

    Added functionality

--- pycalc.py
+++ pycalc.py
@@ -3,7 +3,7 @@
 """PyCalc is a simple calculator built with Python and PyQt."""
 
 import sys  # imports sys. This module provides the exit() function, which we use to terminate.
-
+from functools import partial
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QApplication,
@@ -17,9 +17,11 @@ from PyQt6.QtWidgets import (
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget  #  imports the required classes from PyQt6.QtWidgets.
 
+ERROR_MSG = "ERROR"
 WINDOW_SIZE = 235  # creates a Python constant to hold a fixed window size in pixels for our calculator app.
 DISPLAY_HEIGHT = 35
 BUTTON_SIZE = 40
+
 
 class PyCalcWindow(QMainWindow):  # creates the PyCalcWindow class to provide the app’s GUI. This class inherits from QMainWindow.
     """PyCalc's main window (GUI or view)."""
@@ -73,11 +75,48 @@ class PyCalcWindow(QMainWindow):  # creates the PyCalcWindow class to provide th
         """Clear the display."""
         self.setDisplayText("")
 
+def evaluateExpression(expression):
+    """Evaluate an expression (Model)."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+    return result
+
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
 def main():
     """PyCalc's main function."""
     pycalcApp = QApplication([])  # creates a QApplication object named pycalcApp
     pycalcWindow = PyCalcWindow()  # creates an instance of the app’s window, pycalcWindow
     pycalcWindow.show()  # shows the GUI by calling .show() on the window object.
+    PyCalc(model=evaluateExpression, view=pycalcWindow)
     sys.exit(pycalcApp.exec())  # runs the application’s event loop with .exec().
 
 if __name__ == "__main__":
